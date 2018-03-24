17422a1684d304f337614bc415ab1f611c4ccb9e
FireMaxXu <yifax10@uci.edu>
2018-03-23 17:47:27 -0700

    Fix Branch Bug
    Simplified Branch Unit to avoid possible control signal bug

--- design/BranchUnit.sv
+++ design/BranchUnit.sv
@@ -13,6 +13,7 @@
 // 
 // Dependencies: 
 // 
+// Revision: 0.05 - Fix Jalr Select Bug
 // Revision: 0.04 - Fix Flush logic
 // Revision: 0.03 - Fix Width Bug
 // Revision: 0.02 - Fix PcSel Bug
@@ -36,18 +37,15 @@ module BranchUnit
 
     logic Branch_Sel;
     logic [31:0] PC_Full;
-    logic [1:0] New_PC_Sel;
 
     assign PC_Full = {23'b0, Cur_PC};
     assign PC_Imm = PC_Full + Imm;
     assign PC_Four = PC_Full + 32'b100;
 
     assign Branch_Sel = Branch && AluResult[0]; // 0:Branch is taken; 1:Branch is not taken
-    assign New_PC_Sel[0] = JalrSel;
-    assign New_PC_Sel[1] = Branch_Sel;
     
-    assign BrPC = (New_PC_Sel==2'b11) ? AluResult :     // 11:Jalr is taken
-                  (New_PC_Sel==2'b10) ? PC_Imm : 32'b0;  // 10:Branch/Jal is tanekn
-    assign PcSel = (New_PC_Sel==2'b11) || (New_PC_Sel==2'b10);  // 1:branch is taken; 0:branch is not taken(choose pc+4)
+    assign BrPC = (JalrSel) ? AluResult :     // Jalr -> AluResult
+                  (Branch_Sel) ? PC_Imm : 32'b0;  // Branch/Jal -> PC+Imm   // Otherwise, BrPC value is not important
+    assign PcSel = JalrSel || Branch_Sel;  // 1:branch (b/jal/jalr) is taken; 0:branch is not taken(choose pc+4)
 
 endmodule
