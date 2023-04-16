e34165ab9678a8d3a74528255df86b43166205fe
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-16 03:08:43 -0300

    update datamemory logic

--- design/datamemory.sv
+++ design/datamemory.sv
@@ -22,13 +22,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module datamemory#(
-    parameter DM_ADDRESS = 9 ,
     parameter DATA_W = 32
     )(
     input logic clk,
     input logic MemRead , // comes from control unit
     input logic MemWrite , // Comes from control unit
-    input logic [ DM_ADDRESS -1:0] a , // Read / Write address - 9 LSB bits of the ALU output
+    input logic [ DATA_W -1:0] Instr, // Instruction
+    input logic [ DATA_W -1:0] register_file [31:0], // Register File
     input logic [ DATA_W -1:0] wd , // Write Data
     input logic [2:0] Funct3, // bits 12 to 14 of the instruction
     output logic [ DATA_W -1:0] rd // Read Data
@@ -40,8 +40,8 @@ module datamemory#(
     wire [31:0] Dataout;
     wire Wr;
 
-    assign raddress = {{22{1'b0}}, a};
-    assign waddress = {{22{1'b0}}, a};
+    assign raddress = {{22{1'b0}}, (register_file[Instr[19:15]] + Instr[31:20])}; // This is: 00000000000000000000000000000000{rs1 + imm}
+    assign waddress = {{22{1'b0}}, (register_file[Instr[19:15]] + Instr[31:20])};
     assign Datain = wd;
     assign Wr = MemWrite;
     integer fd;
@@ -61,20 +61,21 @@ module datamemory#(
         begin
             case(Funct3)
             3'b000: //LB
-                rd = {{24{Dataout[7]}}, Dataout[7:0], {8{1'b0}}};
+                rd = {Dataout[7]? 24'hFFFFFF:24'b0, Dataout[7:0]};
             3'b001: //LH
-                rd = {{16{Dataout[15]}}, Dataout[15:0], {16{1'b0}}};
+                rd = {Dataout[15]? 16'hFFFF:16'b0, Dataout[15:0]};
             3'b010: //LW
                 rd = Dataout;
             3'b100: //LBU
-                rd = {{24{1'b0}}, Dataout[7:0]};
+                rd = {24'b0, Dataout[7:0]};
             3'b101: //LHU
-                rd = {{16{1'b0}}, Dataout[15:0]};
+                rd = {16'b0, Dataout[15:0]};
             default:
                 rd = Dataout;
             endcase
+
             fd = $fopen("resultData.txt", "a");
-            $fwrite(fd, "Read value: [%X] | [%d]\nDataOut: %X", rd, rd, Dataout);
+            $fwrite(fd, "Read value: [%X] | [%b]\n", rd, rd);
             $fclose(fd);
         end
     end
